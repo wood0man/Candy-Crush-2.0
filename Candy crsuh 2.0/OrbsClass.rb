df4f6d4af1895f995c77e20054c47cd044f4eb39
wood0man <wood0man@outlook.com>
2024-01-28 15:37:42 +0300

    Uploaded this new version

---
+++ Candy crsuh 2.0/OrbsClass.rb
@@ -0,0 +1,44 @@
+class Orb
+#initialize the colors with color
+   
+    attr_accessor :color
+
+    def initialize(color=nil)
+        if (color.nil?)
+            case rand(1..5)
+            when 1
+                @color="Red"
+    
+            when 2
+                @color="Blue"
+    
+            when 3
+                @color="Yellow"
+    
+            when 4
+                @color="Green"
+    
+            when 5
+                @color="Purple"
+            end
+        else
+            @color=color
+        end
+    
+    end
+    
+    
+    def to_s
+        @color
+    end
+    
+    def ==(other)
+        if other == nil
+            return false
+        else
+        end
+        color==other.color()
+    
+    end
+end
+
---
+++ Candy crsuh 2.0/testing.rb
@@ -0,0 +1,9 @@
+a=[1,2,3,4,5,6]
+a.each_with_index(){|e,i|
+if (i >2)
+    break
+else
+
+puts(e) 
+end
+}
